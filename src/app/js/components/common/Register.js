4b06b9d7a972a17de6a19a21fac12f066a109c27
lijunwan <879802037@qq.com>
2016-03-20 22:36:03 +0800

    添加注册组件

---
+++ src/app/js/components/common/Register.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes } from 'react';
+export default class Register extends Component {
+	constructor(props) {
+	    super(props);
+    }
+    render(
+    	<p>Register</p>
+    )
+}
