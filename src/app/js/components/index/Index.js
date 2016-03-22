5c19e9351c3315f4520e8ba8f5b18bc62fee4306
万俐君 <lijun.wan@socialcredits.cn>
2016-03-22 18:02:36 +0800

    搜索

--- src/app/js/components/index/Index.js
+++ src/app/js/components/index/Index.js
@@ -1,33 +1,16 @@
 import React, { Component, PropTypes } from 'react';
-//import  '../../../scss/index.scss';
+import logoImg from '../../../images/logo.jpg';
+import '../../../css/index.css';
 export default class  Index extends Component{
-	scrollUp(name){
-       location.hash=name
-	}
 	render(){
 		return(
-			<div>
-			<div className="div">
-					<div className="testDiv">
-						<p id="a">a</p>
-						<p id="d">d</p>
-						<p id="e">e</p>
-					</div>
-					<div className="testDiv">
-						<p id="b">b</p>
-					</div>
-					<div className="testDiv">
-						<p id="c">c</p>
-					</div>
+			<div className="Index">
+				<div className="Index-header">
+					<img src={logoImg} alt=""/>
+					<input placeholder="请输入图书名或者作者名字进行搜索"/>
+					<a href="javascript:;"><i className="fa fa-search"></i></a>
 				</div>
-				<ul>
-					<li><a name="a" onClick={this.scrollUp.bind(this,"a")}>a</a></li>
-					<li><a name="b" onClick={this.scrollUp.bind(this,"b")}>b</a></li>
-					<li><a name="b" onClick={this.scrollUp.bind(this,"c")}>c</a></li>
-					<li><a name="b" onClick={this.scrollUp.bind(this,"d")}>d</a></li>
-					<li><a name="c" onClick={this.scrollUp.bind(this,"e")}>e</a></li>
-				</ul>
-				</div>
-				)
+			</div>
+	  )
 	}
 }
