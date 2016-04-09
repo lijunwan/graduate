e00bbd80c64e0a3562b2057e006b72d10f19955b
changhuali <1275305684@qq.com>
2016-04-09 06:42:39 -0400

    调整大致布局

--- src/app/js/components/index/Index.js
+++ src/app/js/components/index/Index.js
@@ -2,7 +2,14 @@ import React, { Component, PropTypes } from 'react';
 import logoImg from '../../../images/logo.jpg';
 import '../../../css/index.css';
 import Search from '../common/Search';
-import BookMenu from './BookMenu'
+import BookMenu from './BookMenu';
+import { Carousel } from 'antd';
+import sliderImg1 from '../../../images/slider1.jpg';
+import sliderImg2 from '../../../images/slider2.jpg';
+import sliderImg3 from '../../../images/slider3.jpg';
+import sliderImg4 from '../../../images/slider4.jpg';
+import sliderImg5 from '../../../images/slider5.jpg';
+
 export default class  Index extends Component{
 	constructor(props){
 		super(props)
@@ -10,13 +17,31 @@ export default class  Index extends Component{
 	render(){
 		return(
 			<div className="Index">
-				<div className="Index-header">
+				<div className="Index-header clearfix">
 					<img src={logoImg} alt=""/>
 					<div className="Index-search">
 						<Search {...this.props} />
 					</div>
 				</div>
-				<BookMenu {...this.props} />
+				<div className="clearfix Index-container">
+					<div className="Index-bookMenu">
+						<BookMenu {...this.props} />
+					</div>
+					<div className="Index-center">
+						<div className="Index-slider">
+							<Carousel autoplay="true">
+								<div><img src={sliderImg1} /></div>
+								<div><img src={sliderImg2} /></div>
+								<div><img src={sliderImg3} /></div>
+								<div><img src={sliderImg4} /></div>
+								<div><img src={sliderImg5} /></div>
+							</Carousel>
+						</div>
+					</div>
+					<div className="Index-right">
+						
+					</div>
+			  </div>
 			</div>
 	  )
 	}
