5c2acb88230295869b697f99bafcfbfd5d391a31
changhuali <1275305684@qq.com>
2016-04-09 10:06:04 -0400

    图书分类添加数据

--- src/app/js/dict.js
+++ src/app/js/dict.js
@@ -1,68 +1,508 @@
 var config = {
-	'bookMenu':[{
-	flag:'J',
-	name:'教育',
-	children:[
-		{
-			flag:'J',
-			name:'教材',
-			children:[
-				{
-					flag:'JG',
-					name:'高职高专教材'
-				},
-				{
-					flag: 'JZ',
-					name: '中职教材'
-				}
-			]
-		},
-		{
-			flag:'W',
-			name:'外语',
-			children:[
-				{
-					flag:'WK',
-					name:'口语'
-				}
-			]
-		}
-	]
-},
-{
-	flag: 'W' ,
-	name: '文艺',
-	children:[
-		{
-			flag: 'WW',
-			name: '文学',
-			children: [
-				{
-					flag: 'WWW',
-					name: '文集',
-				},
-				{
-					flag: 'WWG',
-					name: '纪实文学',
-				}
-			]
-		},
-		{
-			flag: 'WZ',
-			name: '传记',
-			children:[
-				{
-					flag: 'WZC',
-					name: '财经人物',
-				},
-				{
-					flag: 'WZZ',
-					name: '政治人物',
-				}
-			]
-		}
-	]
-	}
-	]
+	'bookMenu':[
+	{
+		flag:'A',
+		name:'教育',
+		children:[
+			{
+				flag:'AA',
+				name:'教材',
+				children:[
+					{
+						flag:'AAA',
+						name:'研究生/本科'
+					},
+					{
+						flag:'AAB',
+						name:'高职高专教材'
+					},
+					{
+						flag: 'AAC',
+						name: '中职教材'
+					},
+					{
+						flag: 'AAD',
+						name: '成人教育教材'
+					},
+					{
+						flag: 'AAE',
+						name: '职业技术培训'
+					},
+					{
+						flag: 'AAF',
+						name: '公共课'
+					},
+					{
+						flag: 'AAG',
+						name: '经济管理类'
+					},
+					{
+						flag: 'AAH',
+						name: '工学类'
+					},
+					{
+						flag: 'AAI',
+						name: '文法类'
+					},
+					{
+						flag: 'AAJ',
+						name: '医学类'
+					},
+					{
+						flag: 'AAK',
+						name: '理学类'
+					},
+					{
+						flag: 'AAI',
+						name: '农学'
+					},
+
+				]
+			},
+			{
+				flag:'AB',
+				name:'外语',
+				children:[
+					{
+						flag:'ABA',
+						name:'英语专项训练'
+					},
+					{
+						flag:'ABB',
+						name:'英语读物'
+					},
+					{
+						flag:'ABC',
+						name:'英语考试'
+					},
+					{
+						flag:'ABD',
+						name:'小语种'
+					},
+					{
+						flag:'ABE',
+						name:'日语'
+					},
+					{
+						flag:'ABF',
+						name:'法语'
+					},
+					{
+						flag:'ABF',
+						name:'韩语'
+					},
+				]
+			},
+			{
+				flag: 'AC',
+				name: '考试',
+				children:[
+					{
+						flag: 'ACA',
+						name: '学历考试',
+					},
+					{
+						flag: 'ACB',
+						name: '公务员',
+					},
+					{
+						flag: 'ACC',
+						name: '财税外贸保险',
+					},
+					{
+						flag: 'ACD',
+						name: '计算机',
+					},
+					{
+						flag: 'ACE',
+						name: '建筑工程',
+					},
+					{
+						flag: 'ACF',
+						name: '医药卫生',
+					},
+					{
+						flag: 'ACG',
+						name: '艺术/体育',
+					},
+					{
+						flag: 'ACH',
+						name: '考研',
+					},
+					{
+						flag: 'ACI',
+						name: 'MBA/MPA/MPAc',
+					},
+					{
+						flag: 'ACJ',
+						name: '会计',
+					},
+					{
+						flag: 'ACK',
+						name: '建造师',
+					},
+					{
+						flag: 'ACL',
+						name: '医师资格',
+					},
+					{
+						flag: 'ACM',
+						name: '人力资源管理',
+					},
+				]
+			},
+			{
+				flag: 'AD',
+				name: '中小学教辅',
+				children: [
+					{
+						flag: 'ADA' ,
+						name: '小学' ,
+					},
+					{
+						flag: 'ADB' ,
+						name: '初中' ,
+					},
+					{
+						flag: 'ADC' ,
+						name: '高中' ,
+					},
+					{
+						flag: 'ADD' ,
+						name: '中小学阅读' ,
+					},
+					{
+						flag: 'ADE' ,
+						name: '英语专项' ,
+					},
+					{
+						flag: 'ADF' ,
+						name: '语文作文' ,
+					},
+					{
+						flag: 'ADG' ,
+						name: '工具书' ,
+					},
+					{
+						flag: 'ADH' ,
+						name: '写字/字帖' ,
+					},
+					{
+						flag: 'ADI' ,
+						name: '学习方法' ,
+					},
+					{
+						flag: 'ADJ' ,
+						name: '教育理论' ,
+					},
+				]
+			}
+		]
+	},
+	{
+		flag: 'B',
+		name: '小说',
+		children: [
+			{
+				flag: 'BA',
+				name: '名著',
+				children: [
+					{
+						flag: 'BAA',
+						name: '中国当代小说'
+					},
+					{
+						flag: 'BAB',
+						name: '中国近现代小说'
+					},
+					{
+						flag: 'BAC',
+						name: '中国古典小说'
+					},
+					{
+						flag: 'BAD',
+						name: '世界名著'
+					},
+				]
+			},
+			{
+				flag: 'BB',
+				name: '网络小说',
+				children:[
+					{
+						flag: 'BBA',
+						name: '侦探/悬疑/推理',
+					},
+					{
+						flag: 'BBB',
+						name: '惊悚/恐怖',
+					},
+					{
+						flag: 'BBC',
+						name: '魔幻',
+					},
+					{
+						flag: 'BBD',
+						name: '科幻',
+					},
+					{
+						flag: 'BBE',
+						name: '武侠',
+					},
+					{
+						flag: 'BBF',
+						name: '军事',
+					},
+					{
+						flag: 'BBG',
+						name: '情感',
+					},
+					{
+						flag: 'BBH',
+						name: '都市',
+					},
+				],
+			}
+		]
+	},
+	{
+		flag: 'C' ,
+		name: '文艺',
+		children:[
+			{
+				flag: 'CA',
+				name: '文学',
+				children: [
+					{
+						flag: 'CAA',
+						name: '文集',
+					},
+					{
+						flag: 'CAB',
+						name: '纪实文学',
+					},
+					{
+						flag: 'CAC',
+						name: '文学理论',
+					},
+					{
+						flag: 'CAD',
+						name: '中国古诗词',
+					},
+					{
+						flag: 'CAE',
+						name: '中国当代诗集',
+					},
+					{
+						flag: 'CAF',
+						name: '随笔',
+					},
+					{
+						flag: 'CAG',
+						name: '戏剧',
+					},
+					{
+						flag: 'CAH',
+						name: '民间文学',
+					},
+				]
+			},
+			{
+				flag: 'CB',
+				name: '传记',
+				children:[
+					{
+						flag: 'WZC',
+						name: '财经人物',
+					},
+					{
+						flag: 'WZZ',
+						name: '历代帝王',
+					},
+					{
+						flag: 'WZZ',
+						name: '领袖首脑',
+					},
+					{
+						flag: 'WZZ',
+						name: '军事人物',
+					},
+					{
+						flag: 'WZZ',
+						name: '政治人物',
+					},
+					{
+						flag: 'WZZ',
+						name: '历史人物',
+					},
+					{
+						flag: 'WZZ',
+						name: '女性人物',
+					},
+					{
+						flag: 'WZZ',
+						name: '法律人物',
+					},
+					{
+						flag: 'WZZ',
+						name: '宗教人物',
+					},
+					{
+						flag: 'WZZ',
+						name: '哲学家',
+					},
+					{
+						flag: 'WZZ',
+						name: '教育家',
+					},
+					{
+						flag: 'WZZ',
+						name: '文学家',
+					},
+					{
+						flag: 'WZZ',
+						name: '艺术家',
+					},
+					{
+						flag: 'WZZ',
+						name: '明星',
+					},
+				]
+			},
+			{
+				flag: 'CD',
+				name: '艺术',
+				children:[
+					{
+						flag: 'CDA',
+						name: '艺术理论',
+					},
+					{
+						flag: 'CDB',
+						name: '绘画',
+					},
+					{
+						flag: 'CDC',
+						name: '书法/篆刻',
+					},
+					{
+						flag: 'CDE',
+						name: '影视/媒体艺术',
+					},
+					{
+						flag: 'CDF',
+						name: '戏剧艺术/舞台艺术',
+					},
+					{
+						flag: 'CDG',
+						name: '音乐',
+					},
+					{
+						flag: 'CDH',
+						name: '舞蹈',
+					},
+					{
+						flag: 'CDJ',
+						name: '雕塑',
+					},
+					{
+						flag: 'CDK',
+						name: '设计',
+					},
+					{
+						flag: 'CDL',
+						name: '人体艺术',
+					},
+					{
+						flag: 'CDM',
+						name: '工艺美术',
+					},
+					{
+						flag: 'CDN',
+						name: '明间艺术',
+					},
+					{
+						flag: 'CDO',
+						name: '建筑艺术',
+					},
+					{
+						flag: 'CDP',
+						name: '艺术类考试',
+					},
+				]
+			},
+			{
+				flag: 'CE',
+				name: '摄影',
+				children:[
+					{
+						flag: 'WZC',
+						name: '摄影理论',
+					},
+					{
+						flag: 'WZZ',
+						name: '分类摄影',
+					},
+					{
+						flag: 'WZZ',
+						name: '摄影器材',
+					},
+					{
+						flag: 'WZZ',
+						name: '技法/教程',
+					},
+					{
+						flag: 'WZZ',
+						name: '后期处理',
+					},
+					{
+						flag: 'WZZ',
+						name: '作品集/作品',
+					},
+				]
+			},
+		]
+	},
+	{
+		flag: 'D',
+		name: '青春',
+		children:[],
+	},
+	{
+		flag: 'E',
+		name: '童书',
+		children:[],
+	},
+	{
+		flag: 'F',
+		name: '生活',
+		children:[],
+	},
+	{
+		flag: 'G',
+		name: '人文社科',
+		children:[],
+	},
+	{
+		flag: 'H',
+		name: '经管',
+		children:[],
+	},
+	{
+		flag: 'I',
+		name: '励志/成功',
+		children:[],
+	},
+	{
+		flag: 'J',
+		name: '科技',
+		children:[],
+	},
+	{
+		flag: 'K',
+		name: '工具书',
+		children:[],
+	},
+]
 }
 module.exports = config;
