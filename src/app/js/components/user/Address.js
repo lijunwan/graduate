f22e2d8dcc7ba1d5a20ea1886c1d64e3a0501045
changhuali <1275305684@qq.com>
2016-04-30 01:09:27 -0400

    修改地址界面

--- src/app/js/components/user/Address.js
+++ src/app/js/components/user/Address.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
+import {Row, Col} from 'antd';
 export default class  Address extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return(
-      <div>Address</div>
+      <div className="Address">
+        <h2>收货地址管理</h2>
+        <Row>
+            <Col span="2" style={{textAlign: 'right', marginRight: '10px'}}>选择收货地区</Col>
+            <Col span="20"></Col>
+        </Row>
+        <Row>
+            <Col span="2" style={{textAlign: 'right', marginRight: '10px'}}><label>详细地址</label></Col>
+            <Col span="10">
+                <input className="ant-input" />
+            </Col>
+        </Row>
+        <Row>
+            <Col span="2" style={{textAlign: 'right', marginRight: '10px'}}><label>邮政编码</label></Col>
+            <Col span="10">
+                <input className="ant-input" />
+            </Col>
+        </Row>
+        <Row>
+            <Col span="2" style={{textAlign: 'right', marginRight: '10px'}}><label>收货人姓名</label></Col>
+            <Col span="10">
+                <input className="ant-input" />
+            </Col>
+        </Row>
+        <Row>
+            <Col span="2" style={{textAlign: 'right', marginRight: '10px'}}><label>手机号</label></Col>
+            <Col span="10">
+                <input className="ant-input" />
+            </Col>
+        </Row>
+        <input className="Address-save" value="保存" />
+      </div>
     )
   }
 }
